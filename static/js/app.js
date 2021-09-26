8627152817c70f58e7c154ef25914e435fb20db9
Claira J <julia.claira@gmail.com>
2021-09-26 10:35:16 -0700

    added additional graphs

--- static/js/app.js
+++ static/js/app.js
@@ -173,9 +173,17 @@ d3.csv(usVaccURL).then((importedData)=>{
             myCounties.forEach((value) => {
                 idFieldCounty.append("option").text(value).attr('value',value);
             });
-            
+            topFive(myCounties)
         });
         d3.select("#county").html("<h2>Select Your County</2>");
+        
+    }
+
+    //function to find top five counties
+    function topFive(counties){
+
+        console.log(counties)
+
     }
 
         //finds county info -- start here
@@ -188,17 +196,17 @@ d3.csv(usVaccURL).then((importedData)=>{
                 data1.forEach(countyObject => {
                     
                     if (countyObject.county===idFieldCounty.property("value")){
-                        myCounties.push(countyObject)
+                    myCounties.push(countyObject)
                     }
                 });
                 d3.select("#county").html("");
-                createLine(myCounties,"county");
+                createLine(myCounties,"bar");
             });
         }
 
-    //Finds all the objects for the state -- I'm going to append to take county info also
-    function stateInformation() { 
-        
+    //Finds all the objects for the state and county
+  function stateInformation() { 
+        console.log(`this: ${this.id}`);
         var theStateinfo=[];//an array holding all the state information
         data.forEach(stateObject => {
             
@@ -213,8 +221,8 @@ d3.csv(usVaccURL).then((importedData)=>{
             idFieldCounty.html("").html("");
         }
         else {
-            createLine(theStateinfo,"bar");
-            if (this.type!='checkbox') findCounties();
+            createLine(theStateinfo,"bar",this.id);
+            findCounties();
         }
 
     }
@@ -222,18 +230,17 @@ d3.csv(usVaccURL).then((importedData)=>{
 
 
     //LINE GRAPH DRAW
-    function createLine(myState,graphLocation){
-        if (graphLocation==="county") {
+    function createLine(myState,graphLocation,myEvent){
+        if (graphLocation==="county") {//this is old as now the graph is drawn in the same place
             var titleLocation=idFieldCounty.property("value");
-            var graphColors=['green','purple'];
-            var metaLocation ="#sample-metadata-county";
-            var zoom ="#zoom-2";
+            var metaLocation ="#sample-metadata";
+            //var zoom ="#zoom-2";
         }
         else {
             var titleLocation=states[idField.property("value")];
             var graphColors=['blue','orange'];
             var metaLocation ="#sample-metadata";
-            var zoom ="#zoom";
+            //var zoom ="#zoom";
         }
         //if the last entry doesn't have values yet, this eliminates it
         var arrayLength=myState.length-1;
@@ -245,25 +252,57 @@ d3.csv(usVaccURL).then((importedData)=>{
         }
 
         //checks to see if zoom is on
-        if(d3.select(zoom).property('checked')===false){
-            var startPoint=0;//includes the whole period
-        }
-        else {
-            var startPoint=arrayLength-60;//will just look for the last two months;
-        };
+        //if(d3.select(zoom).property('checked')===false){
+         //   var startPoint=0;//includes the whole period
+        //}
+        //else {
+        //    var startPoint=arrayLength-90;//will just look for the last two months;
+       // };
 
         //slices out information depending on if zoom is checked
         var lastEntry=myState[arrayLength];//finds last entry with values
         var myStateSelection=myState;
-        myStateSelection=myStateSelection.slice(startPoint,arrayLength+1);
+        myStateSelection=myStateSelection.slice(0,arrayLength+1);
 
       
     
         //Creates Trace
+        var selectorOptions = {
+            buttons: [{
+                step: 'month',
+                stepmode: 'backward',
+                count: 1,
+                label: '1m'
+            }, {
+                step: 'month',
+                stepmode: 'backward',
+                count: 3,
+                label: '3m'
+            },
+                {
+                step: 'month',
+                stepmode: 'backward',
+                count: 6,
+                label: '6m'
+            }, {
+                step: 'year',
+                stepmode: 'todate',
+                count: 1,
+                label: 'YTD'
+            }, {
+                step: 'year',
+                stepmode: 'backward',
+                count: 1,
+                label: '1y'
+            }, {
+                step: 'all',
+            }],
+        };
         var trace2 = {
             type: 'line',
             line: {
-                color: graphColors[0]
+                width: 1.2,
+                color: "#277da1"
             },
             x: myStateSelection.map(row=>row['date']),
             y: myStateSelection.map(row=>row['actuals.newCases']),
@@ -275,7 +314,8 @@ d3.csv(usVaccURL).then((importedData)=>{
         var trace1 = {
             type: 'line', 
             line: {
-                color: graphColors[1],
+                color: "#f3722c",
+                width: 1.2
               },
             showInLegend: true,
             x: myStateSelection.map(row=>row['date']),
@@ -292,34 +332,40 @@ d3.csv(usVaccURL).then((importedData)=>{
         layout = {
             title: {text:`${titleLocation} Covid Info`},
             title_x: 0,
-            autosize: false,
-            width: 1000,
-            height: 500,
+            autosize: true,
             margin: {
-                l: 70,
-                r: 200,
-                b: 100,
+                l: 100,
+                r: 70,
+                b: 50,
                 t: 30,
-                pad: 4
+                pad: 2
             },
             legend: {
                 y: 1,
-                x: 1.1,
+                x: 0.2,
+                xanchor: 'right',
                 bgcolor: 'transparent',
               },
             yaxis: {title: 'New Cases',
-                titlefont: {color: graphColors[0]},
-                tickfont: {color: graphColors[0]},
+                titlefont: {color:"#277da1"},
+                tickfont: {color:"#277da1"},
+                fixedrange: true,
             },
             yaxis2: {
                 title: 'New Deaths',
-                titlefont: {color: graphColors[1]},
-                tickfont: {color: graphColors[1]},
+                titlefont: {color: "#f3722c"},
+                tickfont: {color: "#f3722c"},
                 overlaying: 'y',
                 side: 'right',
                 range: [0,Math.max(...(sortZero(myStateSelection,'actuals.newDeaths')))*3],
-                showgrid: false
+                showgrid: false,
+                fixedrange: true,
+                
             },
+            xaxis: {
+                rangeselector: selectorOptions
+            },
+
             xanchor: 'left',
             display:'none',
             hovermode:'x unified'
@@ -354,23 +400,225 @@ d3.csv(usVaccURL).then((importedData)=>{
         console.log(myStateFourteen);
         var sevenCases=myStateSeven[7]["actuals.cases"]-myStateSeven[0]["actuals.cases"];
         var fourteenCases=myStateFourteen[7]["actuals.cases"]-myStateFourteen[0]["actuals.cases"];
-        var casesChange=100-(fourteenCases/sevenCases*100);
-        if (casesChange>0) {casesChange=`+${casesChange.toFixed(1)}%`}
-        else {casesChange=`${casesChange.toFixed(1)}%`};
+        var casesChangeRaw=100-(fourteenCases/sevenCases*100);
+        var casesChange="";
+        var dateSplit=lastEntry["date"].split("-");
+        var dateArrange=`${dateSplit[1]}/${dateSplit[2]}/${dateSplit[0]}`;
+        if (casesChangeRaw>0) {casesChange=`+${casesChangeRaw.toFixed(1)}%`}
+        else {casesChange=`${casesChangeRaw.toFixed(1)}%`};
 
         var fullyVacc = (+lastEntryVacc["metrics.vaccinationsCompletedRatio"]*100);
         var partVacc = (+lastEntryVacc["metrics.vaccinationsInitiatedRatio"]*100)-fullyVacc;
         console.log(+lastEntry["metrics.vaccinationsCompletedRatio"]+20000);
-        metaHTML=metaHTML+`<p><b>Date:</b> ${lastEntry["date"]}</p>`;
+        metaHTML=metaHTML+`<p><b>Updated:</b> ${dateArrange}</p>`;
         metaHTML=metaHTML+`<p><b>Total Cases:</b> ${commas(lastEntry["actuals.cases"])}</p>`;
-        metaHTML=metaHTML+`<p><b>New Cases Trend:</b> ${casesChange}</p>`;
+       // metaHTML=metaHTML+`<p><b>New Cases Trend:</b> ${casesChange}</p>`;
         metaHTML=metaHTML+`<p><b>Total Deaths:</b> ${commas(lastEntry["actuals.deaths"])}</p>`;
         metaHTML=metaHTML+`<p><b>Fully Vaccinated :</b> ${fullyVacc.toFixed(1)}%</p>`;
         metaHTML=metaHTML+`<p><b>Partially Vaccinated :</b> ${partVacc.toFixed(1)}%</p>`;
-        metaHTML=metaHTML+`<p><b>Risk Level Overall :</b> ${lastEntry["riskLevels.overall"]}</p>`;
+        //metaHTML=metaHTML+`<p><b>Risk Level Overall :</b> ${lastEntry["riskLevels.overall"]}</p>`;
 
         meta.node().innerHTML=metaHTML;
 
+       createGauge(lastEntry["riskLevels.overall"])
+
+
+       //GAUGE GRAPH DRAW
+        function createGauge(theRisk){
+
+            var defAdjust=0;
+            if (theRisk==0 || theRisk==2)defAdjust=10;
+            var RiskLevelDef={0:' Low',1:'Medium',2:' High',3:'Critical',4:'Very High',5:'Extreme'};
+
+            d3.select("#gaugeTitle").html("");
+            d3.select("#gaugeFooter").html("");
+            var gaugeSVG=d3.select("#gaugeTitle").append("svg").attr('height',30).attr('width',200);
+            gaugeSVG.append('text').text('Risk Level').attr("x", 60).attr("y",15)
+            .style("fill","black").attr('font-size','19px');
+
+            var footerSVG=d3.select("#gaugeFooter").append("svg").attr('height',60).attr('width',200);
+            footerSVG.append('text').text(RiskLevelDef[theRisk]).attr("x", 75+defAdjust).attr("y",15)
+            .style("fill","black").attr('font-size','15px').style("font-style","italic")
+
+            var alertColors=["#43aa8b", "#90be6d", "#f9c74f","#f94144", "#f9844a", "white"];
+            var data = [
+                {
+                domain: { x: [0, 1], y: [0, 1] },
+                value: theRisk,
+                type: "indicator",
+                mode: "gauge+number",
+                gauge: {
+                    bar: {
+                        thickness:0.2,
+                        color: "#277da1"  
+                    },
+                    axis: { range: [0, 5],
+                        showticklabels:false,
+                        ticks:false,
+                        visible:false
+                        },
+                    steps: [
+                    { range: [0, 1], color: alertColors[0]},
+                    { range: [1, 2], color: alertColors[1] },
+                    { range: [2, 3], color: alertColors[2] },
+                    { range: [3, 4], color: alertColors[4] },
+                    { range: [4, 5], color: alertColors[3] },
+                    ],
+                    threshold: {
+                    line: { color: "red", width: 4 },
+                    thickness: 0.75,
+                    value: theRisk
+                    }
+                }
+                }
+            ];
+            
+            var layout = { width: 180, height: 75, margin: { l:20,r:0,t: 0, b: 0 } };
+            Plotly.newPlot('gauge', data, layout,{displayModeBar: false});
+        }
+
+        d3.select("#polyarrow").html("");
+        var svg=d3.select("#polyarrow").append("svg").attr("width","200").attr("height","180");
+    
+        
+        //up arrow
+        var arrowColor="";
+        if (casesChangeRaw>=0){arrowColor="#f94144"}
+        else {arrowColor="#43aa8b"};
+        
+        var aM=.13;
+        var aX=48;
+        var aY=8;
+        var aU=[(110*aM)+aX,(450*aM)+aY,(360*aM)+aX,(50*aM)+aY,(610*aM)+aX,(450*aM)+aY];
+
+        if (casesChangeRaw>=0){
+            var upArrow=svg
+            .append('polyline').attr('opacity',1)
+            .attr('points', `${aU[0]} ${aU[1]}, ${aU[2]} ${aU[3]+10}, ${aU[4]} ${aU[5]}`)
+            .style('fill', arrowColor);
+
+            var myBoxLine=svg.append('rect').attr("width", 28).attr("height", 40)
+            .attr("x", 80.2).attr("y", 60).style("fill",arrowColor);
+        }
+        else {
+            aY=10;
+            aD=[(110*aM)+aX,(450*aM)+aY,(360*aM)+aX,(980*aM)+aY,(610*aM)+aX,(450*aM)+aY]
+        
+            var downArrow=svg
+            .append('polyline').attr('opacity',1)
+            .attr('points', `${aD[0]} ${aD[1]}, ${aD[2]} ${aD[3]-30}, ${aD[4]} ${aD[5]}`)
+            .style('fill', arrowColor);
+
+            var myBoxLine=svg.append('rect').attr("width", 28).attr("height", 40)
+            .attr("x", 80.2).attr("y", 30).style("fill",arrowColor);
+        }
+        
+
+        svg.append('text').text(casesChange).attr("x", 50.2).attr("y",140)
+        .style("fill","black")
+        .attr('font-size','24px');
+
+        svg.append('text').text('New Cases Trend').attr("x", 18.2).attr("y",15)
+        .style("fill","black")
+        .attr('font-size','20px');
+
+
+        //icu bar horizontal
+    
+        if(myEvent==="selDataset"){
+            icu=d3.select("#icu").html("");
+            icu=d3.select("#icu").append("svg");
+
+            //header
+            icu.append('text').text('State ICU Beds Capacity').attr("x", 0).attr("y",15)
+                .style("fill","black")
+                .attr('font-size','19px');
+            
+                var icuBox=icu.append('rect').attr("width", 180).attr("height", 30)
+            .attr("x", 14).attr("y", 30).style("stroke","black").style("fill","white") .attr("stroke-width",1);
+
+            
+            var icuCovid=lastEntry["actuals.icuBeds.currentUsageCovid"];
+            var icuNormal=lastEntry["actuals.icuBeds.currentUsageTotal"]-lastEntry["actuals.icuBeds.currentUsageCovid"];
+            var icuTotal=lastEntry["actuals.icuBeds.capacity"];
+
+            console.log(`Total: ${icuTotal}`);
+            console.log(`Total Covid: ${icuCovid}`);
+            console.log(`Total Normal: ${icuNormal}`);
+
+            var icuCovidPerc=icu.append('rect').attr("width", (icuCovid/icuTotal*180)).attr("height", 30)
+            .attr("x", 14).attr("y", 30).style("fill","#577590");
+
+            var icuNormalPerc=icu.append('rect').attr("width", (icuNormal/icuTotal*180)).attr("height", 30)
+            .attr("x", 14+icuCovid/icuTotal*180).attr("y", 30).style("fill","#43aa8b");
+
+            // icu readouts
+            icu.append('rect').attr("width", 10).attr("height", 10)
+            .attr("x", 15).attr("y", 67).style("fill","#577590").style("stroke","black");
+
+            icu.append('text').text(`Covid Usage:  ${icuCovid}`).attr("x", 30).attr("y",78)
+            .style("fill","black")
+            .attr('font-size','15px');
+
+            icu.append('rect').attr("width", 10).attr("height", 10)
+            .attr("x", 15).attr("y", 87).style("fill","#43aa8b").style("stroke","black");
+
+            icu.append('text').text(`Other Usage:  ${icuNormal}`).attr("x", 30).attr("y",98)
+            .style("fill","black")
+            .attr('font-size','15px');
+
+            icu.append('rect').attr("width", 10).attr("height", 10)
+            .attr("x", 15).attr("y", 107).style("fill","white").style("stroke","black");
+
+            icu.append('text').text(`Open Beds:  ${icuTotal-icuNormal-icuCovid}`).attr("x", 30).attr("y",118)
+            .style("fill","black")
+            .attr('font-size','15px');
+        };
+
+        
+
+/*
+        var trace1 = {
+            x: [70],
+            y: ['giraffes'],
+            orientation: 'h',
+            marker: {
+                color: '#f9c74f',
+              },
+            name: 'SF Zoo',
+            type: 'bar',
+          };
+          
+          var trace2 = {
+            x: [12],
+            y: ['giraffes'],
+            orientation: 'h',
+            marker: {
+                color: '#277da1',
+              },
+            name: 'LA Zoo',
+            type: 'bar',
+          };
+
+          var trace3 = {
+            x: [100-70-12],
+            y: ['giraffes'],
+            orientation: 'h',
+            marker: {
+                color: 'gray',
+              },
+            name: 'Total',
+            type: 'bar',
+          };
+          
+          var data = [trace1, trace2,trace3];
+          
+          var layout = {barmode: 'stack'};
+          var layout = { width: 235, height: 110,
+            margin: { l:0,r:0,t: 60, b: 0 },
+            barmode: 'stack'};
+          
+          Plotly.newPlot('icu', data, layout);*/
 
     };
 
@@ -389,11 +637,13 @@ d3.csv(usVaccURL).then((importedData)=>{
     idFieldCounty.on("change", countyData);
     
 
-    var zoomBox= d3.select("#zoom");//
-    zoomBox.on("change", stateInformation);
+    //var zoomBox= d3.select("#zoom");//
+    //zoomBox.on("change", stateInformation);
 
-    var zoomBox2= d3.select("#zoom-2");//
-    zoomBox2.on("change", countyData);
+    //var zoomBox2= d3.select("#zoom-2");//
+    //zoomBox2.on("change", countyData);
+
+
     
 
     var countycode = "data/fips_zip_x.txt";  
